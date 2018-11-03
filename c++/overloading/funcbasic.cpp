a211f64359098355c4018c139bcb1f6917e4f2b0
Dev <ndevendr@visteon.com>
2018-11-03 13:29:12 -0400

    func

---
+++ c++/overloading/funcbasic.cpp
@@ -0,0 +1,23 @@
+/**
+ * Author Devendra Naga (devendra.aaru@gmail.com)
+ * 
+ * License Apache
+ */
+#include <iostream>
+
+int add(int a, int b)
+{
+    return a + b;
+}
+
+double add(double a, double b)
+{
+    return a + b;
+}
+
+int main()
+{
+    std::cout << "add result for ints <1 and 2>:" << add(1, 2) << std::endl;
+    std::cout << "add result for doubles <1.1 and 2.2>:" << add(1.1, 2.2) << std::endl;
+    std::cout << "add result for a double and int <1.1 and 2>:" << add(1.1, 2.0) << std::endl;
+}
