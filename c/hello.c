cc15fd761a692813d7853e696685cde874fe0287
Dev <ndevendr@visteon.com>
2018-11-03 06:00:13 -0400

    hello

---
+++ c/hello.c
@@ -0,0 +1,13 @@
+/**
+ * Author: Devendra Naga (devendra.aaru@gmail.com)
+ * 
+ * License Apache
+ */
+#include <stdio.h>   // header file
+
+int main() // main function definition
+{
+    printf("Hello C\n");     // printf function call
+
+    return 0;   // return statement
+}
