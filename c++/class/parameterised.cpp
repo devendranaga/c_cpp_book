d59648478b87ae28f4d3cff94e8d6d5180d9720b
Dev <ndevendr@visteon.com>
2018-11-04 01:49:42 -0500

    parameterised

---
+++ c++/class/parameterised.cpp
@@ -0,0 +1,29 @@
+/**
+ * Written by Devendra Naga (devendra.aaru@gmail.com)
+ *
+ * License Apache
+ */
+#include <iostream>
+
+class test_class {
+    public:
+        test_class(int f): a1 (f) // assign f to a1
+        {
+        }
+
+        int geta1()
+        {
+            return a1;
+        }
+
+    private:
+        int a1;
+};
+
+int main()
+{
+    // parameterised constructor call
+    test_class test(5);
+
+    std::cout << "a1: " << test.geta1() << std::endl;
+}
