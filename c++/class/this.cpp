0ab0b90d667379c32a69308a458b083a2b95e4d8
Dev <ndevendr@visteon.com>
2018-11-04 02:40:11 -0500

    this

---
+++ c++/class/this.cpp
@@ -0,0 +1,30 @@
+/**
+ * Written by Devendra Naga (devendra.aaru@gmail.com)
+ *
+ * License Apache
+ */
+#include <iostream>
+
+class thisobj {
+    private:
+        int var;
+    public:
+        thisobj()
+        {
+            var = 4;
+        }
+        int getThis()
+        {
+            std::cout << "this val:" << this << std::endl;
+
+            // causes recursive loop because this is class itself and calling the function itself via this indirectly
+            this->getThis();
+        }
+};
+
+int main()
+{
+    thisobj obj;
+
+    obj.getThis();
+}
