6bb90282735f46fe029c65580e45d2a1b062e64b
Dev <ndevendr@visteon.com>
2018-11-04 03:03:34 -0500

    multiline

---
+++ c/multiline.c
@@ -0,0 +1,15 @@
+/**
+ * Author: Devendra Naga (devendra.aaru@gmail.com)
+ *
+ * License Apache
+ */
+#include <stdio.h>
+
+int main()
+{
+    /**
+      Here is a multiline comment
+      for a program that does nothing
+     */
+    return 0;
+}
