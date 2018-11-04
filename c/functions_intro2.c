f9dc2b53764d9c38ed7923c9a564a020c09314d2
Dev <ndevendr@visteon.com>
2018-11-04 07:15:06 -0500

    functiosn

---
+++ c/functions_intro2.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Devendra Naga (devendra.aaru@gmail.com)
+ *
+ * License Apache
+ */
+#include <stdio.h>
+
+int main()
+{
+        double f1 = f();
+    printf("val %f\n", f1);
+}
+
+double f()
+{
+    return 44.1;
+}
