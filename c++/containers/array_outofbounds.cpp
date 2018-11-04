f4327d2ed92a0d9272fc66a5094461de162797d3
Dev <ndevendr@visteon.com>
2018-11-04 12:05:11 -0500

    update

---
+++ c++/containers/array_outofbounds.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <array>
+
+int main()
+{
+    std::array <int, 4> a;
+    int i;
+
+    for (i = 0; i < 10; i ++) {
+        a.at(i) = i;
+    }
+
+    std::array <int, 4> :: const_iterator it;
+
+    i = 0;
+    for (it = a.begin(); it != a.end(); it ++) {
+        std::cout << "array[" << i << "]" << " " << *it << std::endl;
+    }
+
+    return 0;
+}
+
