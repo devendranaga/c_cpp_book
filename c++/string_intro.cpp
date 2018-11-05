7c34c64f5d1b0829ed5c52cddda1fa64f0bd93ed
Dev <ndevendr@visteon.com>
2018-11-05 15:08:24 -0500

    string

---
+++ c++/string_intro.cpp
@@ -0,0 +1,14 @@
+/**
+ * Written by Devendra Naga (devendra.aaru@gmail.com)
+ * 
+ * License Apache
+ */
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string str1 = "Programming";
+
+    std::cout << "first char : " << str1.at(0) << std::endl;
+}
