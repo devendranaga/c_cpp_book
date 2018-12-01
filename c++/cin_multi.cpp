ce3d4649234808a62475fc8d6c7ec5294516b50f
DevNaga <devendra.aaru@gmail.com>
2018-12-01 09:25:56 -0500

    update

---
+++ c++/cin_multi.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    int a, b, c, d;
+
+    std::cin >> a >> b >> c >> d;
+
+    std::cout << " a " << a << std::endl
+              << " b " << b << std::endl
+              << " c " << c << std::endl
+              << " d " << d << std::endl;
+}
+
