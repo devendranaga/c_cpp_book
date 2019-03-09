dc3f7402f907c8798366a062e7e3f2ef9c15a705
Dev <devendra.aaru@gmail.com>
2019-03-08 23:30:02 -0500

    constant description

---
+++ c/constant.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    const int i = 4;
+
+    printf("%d\n", i);
+}
+
---
+++ c/constant_error.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    const int i = 4;
+
+    i = 2;
+
+    printf("i %d\n", i);
+}
+
