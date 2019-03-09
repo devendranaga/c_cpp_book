8ffe08dbebd63e544eb268438d6275daf5dd3df8
Dev <devendra.aaru@gmail.com>
2019-03-09 02:10:22 -0500

    structures

---
+++ c++/structures.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+struct s {
+    private:
+        int a;
+    public:
+        s() { a = 0; }
+        ~s() { }
+
+        int get_a()
+        {
+            return a;
+        }
+
+        void set_a(int a1)
+        {
+            a = a1;
+        }
+};
+
+int main()
+{
+    s obj;
+
+    obj.set_a(4);
+
+    std::cout << obj.get_a() << std::endl;
+}
+
