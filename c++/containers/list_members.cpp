f3ec65e3530f858a2e7ae40c5c8e16057439fd0b
MADMAX <devendra.aaru@gmail.com>
2019-03-29 08:49:34 -0400

    Update list_members.cpp

--- c++/containers/list_members.cpp
+++ c++/containers/list_members.cpp
@@ -12,6 +12,7 @@ int main(int argc, char **argv)
     std::list <std::string> s;
     int i;
 
+    // push all the input command line arguments to list
     for (i = 1; i < argc; i ++) {
 
         std::string t = {argv[i]};
@@ -23,6 +24,7 @@ int main(int argc, char **argv)
 
     std::cout << "print elements " << std::endl;
 
+    // iterate all elements one by one
     for (it = s.begin(); it != s.end(); it ++) {
         std::cout << "it " << *it << std::endl;
     }
